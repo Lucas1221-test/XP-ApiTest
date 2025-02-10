49bbdd95260d2e0ae68271eea1a876a9e72a3504
李静 <jing.li@hexinfo.cn>
2025-02-10 08:33:35 +0000

    Update test_zdgl.py

--- testcase/System management/test_zdgl.py
+++ testcase/System management/test_zdgl.py
@@ -7,7 +7,7 @@ from common.yaml_utils import write_yaml, read_yaml
 from common.files_path import data_path
 from hotloads.debug_talk import DebugTalk
 """测试数据路径"""
-data_path = data_path + 'test_zdgl.yaml'
+data_path = data_path + 'systemmanagementyaml/test_zdgl.yaml'
 @allure.epic("系统管理")
 @allure.feature("系统管理-字典管理")
 class Test:
