17b73af48db718a0a0fce22785eaff27b8829e53
李静 <jing.li@hexinfo.cn>
2025-02-10 08:33:01 +0000

    Update test_sqgl.py

--- testcase/System management/test_sqgl.py
+++ testcase/System management/test_sqgl.py
@@ -7,7 +7,7 @@ from common.yaml_utils import write_yaml, read_yaml
 from common.files_path import data_path
 from hotloads.debug_talk import DebugTalk
 """测试数据路径"""
-data_path = data_path + 'test_sqgl.yaml'
+data_path = data_path + 'systemmanagementyaml/test_sqgl.yaml'
 @allure.epic("系统管理")
 @allure.feature("系统管理-授权管理")
 class Test:
